a00bc4f9e915e11ed4f60eea4002d63f3d9ed4a9
Kent Berry <kentwberry@protonmail.com>
2023-03-11 16:10:50 -0500

    handle validate order response

--- src/main/java/com/berrybeerorderservice/services/BeerOrderManager.java
+++ src/main/java/com/berrybeerorderservice/services/BeerOrderManager.java
@@ -2,7 +2,11 @@ package com.berrybeerorderservice.services;
 
 import com.berrybeerorderservice.domain.BeerOrder;
 
+import java.util.UUID;
+
 public interface BeerOrderManager {
 
     BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+    void processValidationResult(UUID beerOrderId, Boolean isValid);
 }
